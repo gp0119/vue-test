3577ae2a856c1f4ad7d8a428910dee983605d998
ganpeng <190875565@qq.com>
2022-07-01 15:58:22 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <n-input v-model:value="inputValue"></n-input>
+</template>
+
+<script setup lang="ts">
+import { NInput } from 'naive-ui';
+import { ref } from "vue";
+
+const inputValue = ref<string>('Hello World');
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
